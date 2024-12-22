f99be4df22345bf5beb56121e612bdb4408ce8de
iamllcoolray <llcoolray24@gmail.com>
2024-12-22 16:01:48 -0500

    will only track the target user's messages

--- src/bot.py
+++ src/bot.py
@@ -18,20 +18,19 @@ intents.message_content = True # NOQA
 client: Client = Client(intents=intents)
 
 async def send_message(message: Message, user_message: str) -> None:
-    if message.author.name == TARGET_USER:
-        if not user_message:
-            print("(Message was empty because intents were not enabled)")
-            return
+    if not user_message:
+        print("(Message was empty because intents were not enabled)")
+        return
 
-        if is_private := user_message[0] == "?":
-            user_message = user_message[1:]
-        
-        try:
-            response: str = get_bot_response(user_message=user_message)
-            response = f"{message.author.mention} {response}"
-            await message.author.send(response) if is_private else await message.channel.send(response)
-        except Exception as ex:
-            print(ex)
+    if is_private := user_message[0] == "?":
+        user_message = user_message[1:]
+    
+    try:
+        response: str = get_bot_response(user_message=user_message)
+        response = f"{message.author.mention} {response}"
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as ex:
+        print(ex)
             
 @client.event
 async def on_ready() -> None:
@@ -42,12 +41,13 @@ async def on_message(message: Message) -> None:
     if message.author == client.user:
         return
     
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-    
-    print(f"[{channel}] {username}: {user_message}")
-    await send_message(message=message, user_message=user_message)
+    if message.author.name == TARGET_USER:
+        username: str = str(message.author)
+        user_message: str = message.content
+        channel: str = str(message.channel)
+        
+        print(f"[{channel}] {username}: {user_message}")
+        await send_message(message=message, user_message=user_message)
 
 def main() -> None:
     client.run(token=TOKEN)
